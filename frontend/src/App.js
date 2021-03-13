aca8adb16cd43b346fc46914343ac21573194882
Kujtimkras <krasniqii.kujtim@gmail.com>
2021-03-13 21:42:27 +0100

    Home
    Creating Home page, Card, product show

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
-
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
     <>
-     <h1>Welcom to Pro Shop </h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
